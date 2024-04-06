70ebf9f48b8badd7b07f5b62469b4b5862b87dfb
gramaldyhtp <gramaldyhutapea@gmail.com>
2024-04-06 21:09:33 +0700

    Update produk.php

--- produk.php
+++ produk.php
@@ -1,13 +1,19 @@
 <?php
 
-// Jualan Produk
-// Komik
-// Game
 
 class Produk {
-
+     public $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga =  0;
 
 }
 
 $produk1 = new Produk();
+$produk1->judul = "Naruto";
+var_dump($produk1);
+
 $produk2 = new Produk();
+$produk2 ->judul = "Uncharted";
+$produk2->tambahProperty = "hahaha";
+var_dump($produk2);
