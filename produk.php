fc138ac8d6a113d0d0ee07cd40e0de4422996b47
gramaldyhtp <gramaldyhutapea@gmail.com>
2024-04-06 21:35:47 +0700

    produk.php
    method

--- produk.php
+++ produk.php
@@ -7,15 +7,19 @@ class Produk {
             $penerbit = "penerbit",
             $harga =  0;
 
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
 }
 
-    $produk1 = new Produk();
-    $produk1->judul = "Naruto";
-    var_dump($produk1);
-    $produk2 = new Produk();
-    $produk2 ->judul = "Uncharted";
-    $produk2->tambahProperty = "hahaha";
-    var_dump($produk2);
+    //$produk1 = new Produk();
+    //$produk1->judul = "Naruto";
+    //var_dump($produk1);
+    //$produk2 = new Produk();
+    //$produk2 ->judul = "Uncharted";
+    //$produk2->tambahProperty = "hahaha";
+    //var_dump($produk2);
 
 
 $produk3 = new Produk();
@@ -24,4 +28,13 @@ $produk3->penulis = "Masashi Ksihimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
 
-echo "Komik : $produk3->penulis,$produk3->penerbit";
+
+$produk4 = new Produk();
+$produk4 ->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk4->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
